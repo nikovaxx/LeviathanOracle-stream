7e079a1127bd49470fbb109b597836db1d43e888
DisguiseGuy <mastergamer15704@gmail.com>
2026-02-08 20:03:42 +0600

    Embed helper update

--- src/functions/embedHelper.js
+++ src/functions/embedHelper.js
@@ -9,7 +9,7 @@ module.exports = (options = {}) => {
   if (options.url) embed.setURL(options.url);
   if (options.desc || options.description) embed.setDescription(options.desc || options.description);
   if (options.fields) embed.addFields(options.fields);
-  if (options.footer) embed.setFooter({ text: String(options.footer) });
+  if (options.footer) embed.setFooter(typeof options.footer === 'string' ? { text: options.footer } : options.footer);
   if (options.thumbnail) embed.setThumbnail(options.thumbnail);
   if (options.image) embed.setImage(options.image);
   if (options.author) embed.setAuthor(typeof options.author === 'string' ? { name: options.author } : options.author);
