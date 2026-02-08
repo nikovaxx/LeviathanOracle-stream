739d2f542251785031e5c179dc6c60b83da2f89f
DisguiseGuy <mastergamer15704@gmail.com>
2026-02-08 21:02:01 +0600

    DB error fix

--- src/schemas/sqlite3.js
+++ src/schemas/sqlite3.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(path.resolve(__dirname, '..', '..', 'data', 'localdb.db'));
+const db = new Database(path.resolve(__dirname, '..', '..', 'localdb.db'));
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS watchlists (
